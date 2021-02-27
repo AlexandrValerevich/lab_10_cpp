8a3d05a0f347dad51a39700d687360e591514094
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-02-27 11:07:37 +0300

    first commit

---
+++ lab_10_c++/Source.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+#include <clocale>
+#include <ctime>
+#include <iostream>
+
+#define DEBUG
+
+using namespace std;
+
+
+int main() {
+	setlocale(0, "");
+	srand((int)time(NULL));
+	vector<int> integer_v;
+
+	for (int i=0; i < 20; i++)
+		integer_v.push_back(rand()%100);
+
+#ifdef DEBUG
+	for (const auto &it : integer_v) 
+		cout << it << endl;
+	cout << endl;
+#endif // DEBUG
+	
+	sort(integer_v.begin(), integer_v.end(), [](int a, int b) {
+		return a % 2 < b % 2;
+		});
+#ifdef DEBUG
+	for (const auto& it : integer_v)
+		cout << it << endl;
+	cout << endl;
+#endif // DEBUG
+
+	return 0;
+}
